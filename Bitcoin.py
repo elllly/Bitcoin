f919aecbfcd19c0eca364c2c44f86c91aeccf60e
Elaheh <elahehkafashi73@gmail.com>
2022-05-10 23:44:40 +0430

    4th commit

--- Bitcoin.py
+++ Bitcoin.py
@@ -2,6 +2,12 @@ import requests
 from datetime import datetime
 import pytz
 
+def btc_function(a):
+    BTC_Rate.append(data['bpi'][list_data[a]]['rate'])
+    BTC_Rate[a] = int(float(BTC_Rate[a].replace(',', '')))
+    return BTC_Rate[a]
+    
+    
 response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 data = response.json()
 
@@ -13,13 +19,6 @@ print('Requested Time:', datetime_Req_GMT.strftime('%H:%M:%S'))
 list_data = ['USD', 'EUR', 'GBP']
 Symbol = ['\u0024', '\u20ac', '\u00a3']
 BTC_Rate = []
-
-
-def btc_function(a):
-    BTC_Rate.append(data['bpi'][list_data[a]]['rate'])
-    BTC_Rate[a] = int(float(BTC_Rate[a].replace(',', '')))
-    return BTC_Rate[a]
-
 
 for i in range(0, 3):
     print('BTC in', list_data[i], 'at', Time_Site, 'is:', Symbol[i], btc_function(i))
